89a3036c2ae20252305275ebb0fa6fc0f422f370
laifakalon <143606426+laifakalon@users.noreply.github.com>
2024-04-12 23:08:29 +0800

    Update script.js

--- CSQ4_FA1/script.js
+++ CSQ4_FA1/script.js
@@ -1,42 +1,36 @@
-
-function resetdata(){
+function resetdata() {
     var br = "</br>";
 
-
     userDetails.innerHTML = "(Your personal and account details will appear here after you submit.)";
-    document.getElementById ("name").style.border = "1px solid black";
+    document.getElementById("name").style.border = "1px solid black";
     document.getElementById("username").style.border = "1px solid black";
-    document. getElementById ("password").style.border = "1px solid black";
-    document.getElementById( "gender").style.border = "1px solid black";
+    document.getElementById("password").style.border = "1px solid black";
+    document.getElementById("gender").style.border = "1px solid black";
     document.getElementById("age").style.border = "1px solid black";
     document.getElementById("email").style.border = "1px solid black";
     document.getElementById("address").style.border = "1px solid black";
     document.getElementById("interests").style.border = "1px solid black";
     document.getElementById("body").style.overflow = "hidden";
-    document.getElementById ("background").style.height = "100vh";
+    document.getElementById("background").style.height = "100vh";
 
     document.getElementById("data").innerHTML =
-    "Name: " + name + br;
-    "Username: " + username + br;
-    "Gender: " + gender + br;
-    "Password: " + password + br;
-    "Email: " + email + br;
-    "Address: " + address + br;
-    "About the User: " + interests + br;
-
-    return false;
+        "Name: " + Name + br +
+        "Username: " + username + br +
+        "Gender: " + gender + br +
+        "Password: " + password + br +
+        "Email: " + email + br +
+        "Address: " + address + br +
+        "About the User: " + interests + br;
 }
 
-
-
-function blurR() {
-    if (document.getElementsByTagName ("input"). value == "") {
-        document.getElementsByTagName ("input"). style. border = "2px solid red";
+function blurR(inputElement) {
+    if (inputElement.value === "") {
+        inputElement.style.border = "3px solid red";
     } else {
-        document.getElementsByTagName ("input"). style. border = "2px solid #00FF7F";
+        inputElement.style.border = "4px solid #00FF7F";
     }
 }
-    function focusG(){
-    
-        document.getElementsByTagName ("input"). style.border = "4px solid #00FF7F";
-    }
+
+function focusG(inputElement) {
+    inputElement.style.border = "4px solid #00FF7F";
+}
